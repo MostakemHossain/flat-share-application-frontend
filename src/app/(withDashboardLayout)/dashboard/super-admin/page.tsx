5dd2f210d5e4be33aa3fbe5e35a1a23e66d9e96d
MostakemHossain <mostakememon123@gmail.com>
2024-08-01 16:39:02 +0600

    create layout system for different role users

---
+++ src/app/(withDashboardLayout)/dashboard/super-admin/page.tsx
@@ -0,0 +1,7 @@
+const superAdmin = () => {
+  return <div>
+    <h2>super admin</h2>
+  </div>;
+};
+
+export default superAdmin;
---
+++ src/app/(withDashboardLayout)/dashboard/user/page.tsx
@@ -0,0 +1,9 @@
+const userPage = () => {
+  return (
+    <div>
+      <h2>userDashboard</h2>
+    </div>
+  );
+};
+
+export default userPage;
