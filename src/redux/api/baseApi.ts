5a78ed73bff5cb26c75a25771c79100d9e202fb1
MostakemHossain <mostakememon123@gmail.com>
2024-08-01 12:58:11 +0600

    axios implement

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,7 +1,8 @@
+import { axiosBaseQuery } from "@/helpers/axios/axiosBaseQuery";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8500/api/v1" }),
+  baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:8500/api/v1" }),
   endpoints: (builder) => ({}),
 });
