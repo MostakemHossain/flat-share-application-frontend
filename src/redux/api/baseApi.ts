beafbb791a51533cfc71522227f4b11cfd7c011c
MostakemHossain <mostakememon123@gmail.com>
2024-10-20 22:04:09 +0600

    backend intregration

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,10 @@ import { tagTypeList } from "../tag-types";
 
 export const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:8500/api/v1" }),
+  // baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:8500/api/v1" }),
+  baseQuery: axiosBaseQuery({
+    baseUrl: "https://flat-match-backend.vercel.app/api/v1",
+  }),
   endpoints: () => ({}),
-  tagTypes: tagTypeList
+  tagTypes: tagTypeList,
 });
